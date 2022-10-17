e1dfd990dc1d687d362c138b9f08aba9d22881ce
Moondoyeon <mmmdo21@gmail.com>
2022-10-17 21:18:30 +0900

    Feat:할일조회,추가,수정,삭제

---
+++ src/components/Todo/TodoEditor.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { useState, useRef } from "react";
+import styled from "styled-components";
+import DefaultButton from "../../UI/button/DefaultButton";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 5px;
+  button {
+    padding: 5px;
+    margin-top: 5px;
+  }
+`;
+const Title = styled.div`
+  font-weight: 600;
+`;
+export const Textarea = styled.textarea`
+  height: 50px;
+  border-radius: 5px;
+  border: 1px solid #d9d9d9;
+  padding: 5px;
+  font-size: 14px;
+  margin-top: 5px;
+  :focus {
+    outline: 2px solid #95d1cc;
+    border: none;
+  }
+`;
+const TodoEditor = ({ todoList, setUpdatedList }) => {
+  const access_token = JSON.parse(localStorage.getItem("access_token"));
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+  const submitTodo = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}todos`,
+        { todo: content },
+        {
+          headers: { Authorization: `Bearer ${access_token}` },
+        }
+      )
+      .then((res) => {
+        setUpdatedList([...todoList, res.data]);
+        setContent("");
+      })
+      .catch(() => {
+        alert("죄송합니다 다음에 시도해주세요");
+      });
+  };
+  return (
+    <Container>
+      <Title>Todo작성하기</Title>
+      <Textarea
+        placeholder="todo를 입력해주세요"
+        value={content}
+        ref={contentRef}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <DefaultButton text={"작성완료"} type={"mint"} onClick={submitTodo} />
+    </Container>
+  );
+};
+export default TodoEditor;
---
+++ src/components/Todo/TodoItem.js
@@ -0,0 +1,132 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import DefaultButton from "../../UI/button/DefaultButton";
+import { Textarea } from "./TodoEditor";
+const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: 3px 0px;
+  textarea {
+    height: 30px;
+    width: 100%;
+  }
+`;
+const TodoWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  button {
+    margin-left: 3px;
+  }
+`;
+const TodoContent = styled.div``;
+const TodoItem = ({ data, setUpdatedList, todoList }) => {
+  const access_token = JSON.parse(localStorage.getItem("access_token"));
+  const [isEdit, setIsEdit] = useState(false);
+  const [editTodo, setEditTodo] = useState({
+    todo: data.todo,
+    isCompleted: data.isCompleted,
+  });
+  const id = data.id;
+
+  const handleContentChange = (e) => {
+    setEditTodo({ ...editTodo, todo: e.target.value });
+  };
+  const handleIsCompletedChange = (e) => {
+    setEditTodo({ ...editTodo, isCompleted: e.target.checked });
+  };
+
+  const submitEditedTodo = () => {
+    axios
+      .put(`${process.env.REACT_APP_API_URL}todos/${id}`, editTodo, {
+        headers: { Authorization: `Bearer ${access_token}` },
+      })
+      .then(() => {
+        const newList = todoList.map((el) =>
+          el.id === data.id
+            ? {
+                id: data.id,
+                todo: editTodo.todo,
+                isCompleted: editTodo.isCompleted,
+                userId: data.userId,
+              }
+            : el
+        );
+        setUpdatedList(newList);
+        setIsEdit(false);
+      })
+      .catch(() => alert("죄송합니다 잠시후 시도해주세요"));
+  };
+
+  useEffect(() => {
+    submitEditedTodo();
+  }, [editTodo.isCompleted]);
+
+  const deleteTodo = () => {
+    if (window.confirm("정말로 삭제하실건가요?")) {
+      axios
+        .delete(`${process.env.REACT_APP_API_URL}todos/${id}`, {
+          headers: { Authorization: `Bearer ${access_token}` },
+        })
+        .then(() => {
+          const newList = todoList.filter((el) => el.id !== data.id);
+          setUpdatedList(newList);
+        });
+    }
+  };
+  return (
+    <ItemContainer>
+      {isEdit ? (
+        <>
+          <TodoWrapper>
+            <Textarea value={editTodo.todo} onChange={handleContentChange} />
+          </TodoWrapper>
+          <ButtonWrapper>
+            <DefaultButton
+              text={"제출"}
+              type={"mint"}
+              width={"short"}
+              onClick={submitEditedTodo}
+            />
+            <DefaultButton
+              text={"취소"}
+              width={"short"}
+              onClick={() => setIsEdit(false)}
+            />
+          </ButtonWrapper>
+        </>
+      ) : (
+        <>
+          <TodoWrapper>
+            <input
+              type="checkbox"
+              checked={editTodo.isCompleted}
+              onChange={handleIsCompletedChange}
+            />
+            <TodoContent>{editTodo.todo}</TodoContent>
+          </TodoWrapper>
+          <ButtonWrapper>
+            <DefaultButton
+              text={"수정"}
+              type={"mint"}
+              width={"short"}
+              onClick={() => setIsEdit(true)}
+            />
+            <DefaultButton
+              text={"삭제"}
+              type={"red"}
+              width={"short"}
+              onClick={deleteTodo}
+            />
+          </ButtonWrapper>
+        </>
+      )}
+    </ItemContainer>
+  );
+};
+export default TodoItem;
